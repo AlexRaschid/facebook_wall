266277a7de5ca883cc56c37a916c50d854a3373a
Alexander Raschid <alexraschid1337@gmail.com>
2016-04-13 20:32:09 +0000

    added picture/text to comment field

--- starter_code/js/wall.js
+++ starter_code/js/wall.js
@@ -1,6 +1,17 @@
 $(document).ready(function()
 {
 	$('#message').focus();
-
 	// YOUR CODE GOES HERE
+	$("body").append("<div id='pfpmanager'></div>")
+	$("button").click(function(){
+		
+		$("#pfpmanager").prepend("<li><img class='pfpicture' src='http://www.natandmarie.com/wp-content/uploads/2013/06/rsz_facebook.jpg'/>"+$("#message").val()+"</li>");
+			$(".pfpicture").css("length","168px");
+			$(".pfpicture").css("width","168px");
+		var deleteInput = $("#message").attr();
+			if(deleteInput.length > 0)
+				deleteInput === "";
+	});
 });
+
+
